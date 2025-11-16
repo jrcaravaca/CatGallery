1039f985f429c953513fcd6541ece97dea89c930
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-11-16 06:04:59 +0100

    Add: - Fetch funcionando, se cargan las imagenes. - Estilado provisional del grid con las 10 imagenes - Botón "Cargar más" funcionando

--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,10 +1,14 @@
 export function getImages(URL){
-fetch(URL)
-.then(response => {
-    return response.json()
-})
-.then(data => {
-    console.log(data); 
-})
-
+    const gallery = document.getElementById('gallery'); 
+    gallery.innerText = ""; 
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++) {
+                const img = document.createElement('img');
+                img.classList.add('aspect-square', 'max-w-[250px]')
+                img.setAttribute('src',data[i].url)
+                gallery.appendChild(img);
+            }
+        })
 }
--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import './style.css'
 import { URL } from './js/API_Data'; 
 import { getImages } from './js/getImages';
 
-const gallery = document.getElementById('gallery'); 
-const data = getImages(URL); 
+const more_btn = document.getElementById('more_img')
+getImages(URL)
 
-console.log(data)
-for (let i = 0; i < 11; i++) {
-    let img = document.createElement('img'); 
-    
-}
-
+more_btn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    getImages(URL); 
+})
